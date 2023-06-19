e8548ada72c84e92e94a3d04561eaa2a70f5aaf2
Garret Kanwischer <kanwischer.garret@gmail.com>
2023-06-19 12:00:33 -0500

    Added intial db requests for proposals

--- server/routes/proposals.router.js
+++ server/routes/proposals.router.js
@@ -6,22 +6,34 @@ const router = express.Router();
  * GET route template
  */
 router.get('/main', (req, res) => {
-  // GET route code here
-  queryText = `SELECT * FROM "proposal;`;
+    // GET route code here
+    queryText = `SELECT * FROM "proposal";`;
 
-  pool.query(queryText)
-  .then(result => {
-    res.send(result.rows)
-  }).catch(err => {
-    console.log(err);
-  })
+    pool.query(queryText)
+        .then(result => {
+            res.send(result.rows)
+        }).catch(err => {
+            console.log('Error GETing proposals', err);
+            res.sendStatus(500);
+        })
 });
 
 /**
  * POST route template
  */
-router.post('/', (req, res) => {
-  // POST route code here
+router.post('/add-proposal', (req, res) => {
+    // POST route code here
+    
+        const { description } = req.body;
+        const queryText = `INSERT INTO "proposal ( "description", "user_id" )
+                            VALUES ( $1, $2 );`;
+
+        pool.query(queryText, [ description, req.user.id ])
+            .then( res.sendStatus(201) 
+            ).catch(err => {
+                console.log('Error POSTing event', err);
+                res.sendStatus(500);
+            })
 });
 
 module.exports = router;
