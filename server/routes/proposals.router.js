b57b61cf9fb1a301eb52c6d1c551f4827ec31cee
Garret Kanwischer <kanwischer.garret@gmail.com>
2023-06-23 12:01:45 -0500

    Updated the post vote and update-vote to check the vote count

--- server/routes/proposals.router.js
+++ server/routes/proposals.router.js
@@ -50,20 +50,58 @@ router.get('/user', rejectUnauthenticated, (req, res) => {
 });
 
 // post route for submiting a logged-in user's vote on a specific proposal
-router.post('/vote/', rejectUnauthenticated, (req, res) => {
-    const { proposal_id , vote } = req.body
-    const queryText = `INSERT INTO "proposal_vote" ("proposal_id", "user_id", "vote")
-                        VALUES ($1, $2, $3);`;
+// router.post('/vote', rejectUnauthenticated, (req, res) => {
+//     const { proposal_id , vote } = req.body
+//     const queryText = `INSERT INTO "proposal_vote" ("proposal_id", "user_id", "vote")
+//                         VALUES ($1, $2, $3);`;
 
-    pool.query(queryText, [proposal_id, req.user.id, vote])
-        .then(result => {
+//     pool.query(queryText, [proposal_id, req.user.id, vote])
+//         .then(result => {
+//             console.log(`Successful vote cast on proposal id: ${proposal_id} for user: ${req.user.username}`);
+//             res.status(201).send(result.rows);
+//         }).catch(err => {
+//             console.log(`Error casting vote on proposal id: ${proposal_id} for user: ${req.user.username}`, err);
+//             res.sendStatus(500);
+//         })
+// })
+
+router.post('/vote/', rejectUnauthenticated, (req, res) => {
+    const { proposal_id, vote } = req.body;
+  
+    // Insert the vote into the proposal_vote table
+    const insertQuery = `INSERT INTO "proposal_vote" ("proposal_id", "user_id", "vote")
+                          VALUES ($1, $2, $3);`;
+  
+    pool.query(insertQuery, [proposal_id, req.user.id, vote])
+      .then(() => {
+        // Get the vote counts for the proposal
+        const countQuery = `SELECT COUNT(*) FILTER (WHERE "vote" = TRUE) AS "true_votes",
+                                  COUNT(*) FILTER (WHERE "vote" = FALSE) AS "false_votes"
+                            FROM "proposal_vote"
+                            WHERE "proposal_id" = $1;`;
+  
+        pool.query(countQuery, [proposal_id])
+          .then(result => {
+            const { true_votes, false_votes } = result.rows[0];
             console.log(`Successful vote cast on proposal id: ${proposal_id} for user: ${req.user.username}`);
-            res.status(201).send(result.rows);
-        }).catch(err => {
-            console.log(`Error casting vote on proposal id: ${proposal_id} for user: ${req.user.username}`, err);
+            
+            // Check if the number of true or false votes surpasses the specified thresholds
+            if (true_votes > 20) {
+              updateProposalStatus(proposal_id, 'Passed');
+            } else if (false_votes > 5) {
+              updateProposalStatus(proposal_id, 'Vetoed');
+            }
+            
+            res.sendStatus(201);
+          }).catch(err => {
+            console.log(`Error retrieving vote counts for proposal id: ${proposal_id}`, err);
             res.sendStatus(500);
-        })
-})
+          });
+      }).catch(err => {
+        console.log(`Error casting vote on proposal id: ${proposal_id} for user: ${req.user.username}`, err);
+        res.sendStatus(500);
+      });
+  });
 
 // delete route for removing a specified proposal that the logged in user posted
 router.delete('/delete/:id', rejectUnauthenticated, (req, res) => {
@@ -92,19 +130,69 @@ router.get('/user-vote', rejectUnauthenticated, (req, res) => {
 });
 
 // put route for updating a user's vote on a specific proposal_id
-router.put('/update-vote', rejectUnauthenticated, (req,res) => {
-    const { proposal_id, vote } = req.body;
-    const queryText = `UPDATE "proposal_vote" SET "vote" = $1 WHERE "proposal_id" = $2 AND "user_id" = $3;`;
+// router.put('/update-vote', rejectUnauthenticated, (req,res) => {
+//     const { proposal_id, vote } = req.body;
+//     const queryText = `UPDATE "proposal_vote" SET "vote" = $1 WHERE "proposal_id" = $2 AND "user_id" = $3;`;
 
-    pool.query(queryText, [vote, proposal_id, req.user.id])
-    .then((response) => {
-        console.log(`Successfully update vote for proposal id: ${proposal_id}`);
-        res.sendStatus(201);
-      }).catch((err) => {
+//     pool.query(queryText, [vote, proposal_id, req.user.id])
+//     .then((response) => {
+//         console.log(`Successfully update vote for proposal id: ${proposal_id}`);
+//         res.sendStatus(201);
+//       }).catch((err) => {
+//         console.log(`Error updating vote at proposal id: ${proposal_id}`, err);
+//         res.sendStatus(500);
+//       })
+// })
+
+router.put('/update-vote', rejectUnauthenticated, (req, res) => {
+    const { proposal_id, vote } = req.body;
+  
+    // Update the vote in the proposal_vote table
+    const updateQuery = `UPDATE "proposal_vote" SET "vote" = $1 
+                          WHERE "proposal_id" = $2 AND "user_id" = $3;`;
+  
+    pool.query(updateQuery, [vote, proposal_id, req.user.id])
+      .then(() => {
+        // Get the vote counts for the proposal
+        const countQuery = `SELECT COUNT(*) FILTER (WHERE "vote" = TRUE) AS "true_votes",
+                                  COUNT(*) FILTER (WHERE "vote" = FALSE) AS "false_votes"
+                            FROM "proposal_vote"
+                            WHERE "proposal_id" = $1;`;
+  
+        pool.query(countQuery, [proposal_id])
+          .then(result => {
+            const { true_votes, false_votes } = result.rows[0];
+            console.log(`Successfully update vote for proposal id: ${proposal_id}`);
+            
+            // Check if the number of true or false votes surpasses the specified thresholds
+            if (true_votes > 20) {
+              updateProposalStatus(proposal_id, 'Passed');
+            } else if (false_votes > 5) {
+              updateProposalStatus(proposal_id, 'Vetoed');
+            }
+            
+            res.sendStatus(201);
+          }).catch(err => {
+            console.log(`Error retrieving vote counts for proposal id: ${proposal_id}`, err);
+            res.sendStatus(500);
+          });
+      }).catch(err => {
         console.log(`Error updating vote at proposal id: ${proposal_id}`, err);
         res.sendStatus(500);
-      })
-})
+      });
+  });
+
+  function updateProposalStatus(proposalId, status) {
+    const queryText = `UPDATE "proposal" SET "status" = $1 WHERE "id" = $2;`;
+    
+    pool.query(queryText, [status, proposalId])
+      .then(() => {
+        console.log(`Successfully updated status of proposal id ${proposalId} to ${status}`);
+      }).catch(err => {
+        console.log(`Error updating status of proposal id ${proposalId} to ${status}`, err);
+      });
+  }
+  
 
 module.exports = router;
 
