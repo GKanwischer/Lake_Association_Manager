715c4dc5bd3370bafe6ac54060ed72ee741b794f
Garret Kanwischer <kanwischer.garret@gmail.com>
2023-06-26 14:16:48 -0500

    added delete requests to admin saga

--- src/redux/sagas/admin.saga.js
+++ src/redux/sagas/admin.saga.js
@@ -11,8 +11,41 @@ function* fetchUsersSaga() {
     }
 }
 
+function* adminDeleteUserSaga(action){ // expects a user id
+    try {
+        yield axios.delete(`/admin/user-delete/${action.payload}`);
+        console.log(`Successful DELETE request of user id: ${action.payload}`);
+        yield put({ type: 'FETCH_ADMIN_USERS' })
+    } catch (error) {
+        console.log(`Error with DELETE request of user id: ${action.payload}`);
+    }
+}
+
+function* adminDeleteEventSaga(action){ // expects an event id
+    try {
+        yield axios.delete(`/admin/event-delete/${action.payload}`);
+        console.log(`Successful DELETE request of event id: ${action.payload}`);
+        yield put({ type: 'FETCH_EVENTS' })
+    } catch (error) {
+        console.log(`Error with DELETE request of event id: ${action.payload}`);
+    }
+}
+
+function* adminDeletePropSaga(action){ // expects a prop id
+    try {
+        yield axios.delete(`/prop-delete/${action.payload}`);
+        console.log(`Successful DELETE request of prop id: ${action.payload}`);
+        yield put({ type: 'FETCH_PROPS' })
+    } catch (error) {
+        console.log(`Error with DELETE request of prop id: ${action.payload}`);
+    }
+}
+
 function* adminSaga(){
-    yield takeEvery( 'FETCH_ADMIN_USERS' , fetchUsersSaga)
+    yield takeEvery( 'ADMIN_FETCH_USERS' , fetchUsersSaga)
+    yield takeEvery( 'ADMIN_DELETE_USER' , adminDeleteUserSaga)
+    yield takeEvery( 'ADMIN_DELETE_EVENT' , adminDeleteEventSaga)
+    yield takeEvery( 'ADMIN_DELETE_PROP' , adminDeletePropSaga)
 }
 
 export default adminSaga;
