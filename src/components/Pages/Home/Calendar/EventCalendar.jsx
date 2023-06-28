da8d778efd3960ec774411f14203f694c74ae910
Garret Kanwischer <kanwischer.garret@gmail.com>
2023-06-28 11:00:00 -0500

    Tried to change the popover to material, but reverted back to bootstrap for it.

--- src/components/Pages/Home/Calendar/EventCalendar.jsx
+++ src/components/Pages/Home/Calendar/EventCalendar.jsx
@@ -1,13 +1,12 @@
 import React from "react"; // , { useRef }
-import { formatDate } from '@fullcalendar/core'
 import FullCalendar from '@fullcalendar/react' // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import * as bootstrap from "bootstrap";
-// import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import moment from "moment";
 
 
@@ -16,7 +15,6 @@ export default function EventCalendar() {
     const events = useSelector(store => store.events);
 
     useEffect(() => {
-        console.log('component did mount');
         dispatch({ type: 'FETCH_EVENTS' });
     }, []);
 
@@ -25,7 +23,6 @@ export default function EventCalendar() {
             <section id="calendar">
                 <div className="event-calendar" style={{ position: "relative", zIndex: 0 }}>
                     <FullCalendar
-                        // ref = {calendarRef}
                         plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
                         headerToolbar={{
                             left: 'dayGridMonth,timeGridWeek,timeGridDay',
