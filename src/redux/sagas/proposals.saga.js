b1e3b625cf02ca95f72efbac576b6ddeba56090c
Garret Kanwischer <kanwischer.garret@gmail.com>
2023-07-06 20:22:22 -0500

    updated the proposals saga

--- src/redux/sagas/proposals.saga.js
+++ src/redux/sagas/proposals.saga.js
@@ -1,77 +1,93 @@
 import axios from 'axios';
 import { put, takeEvery } from 'redux-saga/effects';
 
+// saga file to manage all of the front end request pertaining to proposals
+
+// saga to get all of the relevant information about all of the proposals
 function* fetchAllPropsSaga() {
     try {
         const response = yield axios.get('/proposals/main');
-        console.log('Successful GET request for all proposals', response);
+        // console.log('Successful GET request for all proposals', response);
+        // set hte props.main reducer
         yield put({ type: 'SET_PROPS', payload: response.data });
     } catch (error) {
-        console.log('Error with GET request for all proposals');
+        // console.log('Error with GET request for all proposals');
     }
 }
 
+// saga to post a new proposal
 function* addPropSaga(action) { // expects { description }
     try {
         yield axios.post('/proposals/add', action.payload);
-        console.log('Succesful POST request for adding a proposal');
+        // console.log('Succesful POST request for adding a proposal');
+        // get the up-to-date information on proposals
         yield put({ type: 'FETCH_USER_PROPS' });
     } catch (error) {
-        console.log('Error with POST request for adding proposal');
+        // console.log('Error with POST request for adding proposal');
     }
 }
 
+// saga to get all of the proposals for the logged in user
 function* fetchUserPropSaga() {
     try {
         const response = yield axios.get('/proposals/user');
-        console.log('Successful GET request for user proposals');
+        // console.log('Successful GET request for user proposals');
+        // set the props.user reducer
         yield put({ type: 'SET_USER_PROPS', payload: response.data })
     } catch (error) {
-        console.log('Error with GET request for user proposals');
+        // console.log('Error with GET request for user proposals');
     }
 }
 
-function* castVoteSaga(action) { // expects { proposal_id and vote(true = pass, false = veto)}
-    try {
-        yield axios.post('proposals/vote', action.payload);
-        console.log('Success POST request for casting vote');
-        yield put({ type: 'FETCH_USER_VOTES' })
-    } catch (error) {
-        console.log('Error with POST request for casting vote');
-    }
-}
-
+// saga to delete a specific proposal created by the logged in user
 function* deletePropSaga(action) { // expects { proposal_id }
     try {
         yield axios.delete(`proposals/delete/${action.payload}`);
-        console.log('Successful DELETE request for a user proposal');
+        // console.log('Successful DELETE request for a user proposal');
+        // get the up-to-date information on user proposals
         yield put({ type: 'FETCH_USER_PROPS' });
     } catch (error) {
-        console.log('Error with DELETE request for a user proposal');
+        // console.log('Error with DELETE request for a user proposal');
     }
 }
 
-function* fetchUserVoteSaga(){
+// saga to get all of the votes on proposals made by the logged in user
+function* fetchUserVoteSaga() {
     try {
         const response = yield axios.get('/proposals/user-vote');
-        console.log('Successful GET request for user votes', response);
+        // console.log('Successful GET request for user votes', response);
+        // set the props.user_votes
         yield put({ type: 'SET_USER_VOTES', payload: response.data });
     } catch (error) {
-        console.log('Error with GET request for user votes');
+        // console.log('Error with GET request for user votes');
     }
 }
 
-function* updateVoteSaga(action){ // expects { payload_id, vote }
+// saga to post a vote on a specified proposal by the logged in user
+function* castVoteSaga(action) { // expects { proposal_id and vote(true = pass, false = veto)}
     try {
-        yield axios.put(`/proposals/update-vote`, action.payload)
-        console.log('Successful PUT request for updating proposal vote');
+        yield axios.post('proposals/vote', action.payload);
+        // console.log('Success POST request for casting vote');
+        // get the up-to-date information on user's votes
         yield put({ type: 'FETCH_USER_VOTES' })
-      } catch (error) {
-        console.log('Error with PUT request for updating proposal vote');
-      }
+    } catch (error) {
+        // console.log('Error with POST request for casting vote');
+    }
 }
 
+// saga update the logged in user's vote on a specific proposal
+function* updateVoteSaga(action) { // expects { payload_id, vote }
+    try {
+        yield axios.put(`/proposals/update-vote`, action.payload)
+        // console.log('Successful PUT request for updating proposal vote');
+        // get the up-to-date information on user's votes
+        yield put({ type: 'FETCH_USER_VOTES' })
+    } catch (error) {
+        // console.log('Error with PUT request for updating proposal vote');
+    }
+}
 
+// watcher saga for the proposal related requests
 function* proposalsSaga() {
     yield takeEvery('FETCH_PROPS', fetchAllPropsSaga)
     yield takeEvery('ADD_PROP', addPropSaga)
